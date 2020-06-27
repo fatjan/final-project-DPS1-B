ff437b03321033d6e079579070b858cef02cd319
juliussin <julius.sintara@gmail.com>
2020-06-28 02:24:46 +0700

    add electronic button to trigger the prediction.

--- jetson_implementation.py
+++ jetson_implementation.py
@@ -7,7 +7,15 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 
 # Set GPIO
+led_pin = [11, 13, 15, 19, 21, 23]
+button_pin = 7
+all_off = [GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH]
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(button_pin, GPIO.IN)
+GPIO.output(led_pin, all_off)
+button_state = GPIO.input(button_pin)
+previous_state = GPIO.input(button_pin)
 
 # Input Model Name
 h5name = input('\n\nInput H5 File Name: ')
@@ -19,6 +27,9 @@ except Exception as e:
     print('\n\nModel not found!!!')
     print(e)
     exit()
+
+# Set Label Name
+label = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 
 # Open Webcam Stream
 cap = cv2.VideoCapture(0)
@@ -62,7 +73,32 @@ while True:
         break
     elif key == ord('p'):
         prediction = model.predict(images.astype(np.float32), batch_size=10)
+        prediction_sort_index = np.argsort(prediction)
+        first_prediction = prediction_sort_index[0][-1]
+        second_prediction = prediction_sort_index[0][-2]
         print(prediction)
+        print('Result: {0}({1:.3f}), {2}({3:.3f})'.format(
+            label[first_prediction],
+            prediction[0][first_prediction],
+            label[second_prediction],
+            prediction[0][second_prediction]
+        ))
+    button_state = GPIO.input(button_pin)
+    print(button_state)
+    if button_state == 0 and previous_state == 1:
+        prediction = model.predict(images.astype(np.float32), batch_size=10)
+        prediction_sort_index = np.argsort(prediction)
+        first_prediction = prediction_sort_index[0][-1]
+        second_prediction = prediction_sort_index[0][-2]
+        print(prediction)
+        print('Result: {0}({1:.3f}), {2}({3:.3f})'.format(
+            label[first_prediction],
+            prediction[0][first_prediction],
+            label[second_prediction],
+            prediction[0][second_prediction]
+        ))
+    previous_state = button_state
 
 cap.release()
 cv2.destroyAllWindows()
+GPIO.cleanup()
